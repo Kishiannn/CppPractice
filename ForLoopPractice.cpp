6f2f4d7f86a81362993755757118b16e510fa2b2
Kishiannn_ <abudachan122@gmail.com>
2025-03-30 19:29:04 +0800

    Create ForLoopPractice.cpp

---
+++ ForLoopPractice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+using std::cout;
+
+int main()
+{
+  cout <<  "Counting Forward" << endl;
+  for(int num; num <= 10; num++){
+
+  cout << num << " ";
+}
+  return 0;
+}
